a2c38b6d6c7778aed3005bce273ee039e422aabe
pocki <2448303+pocki@users.noreply.github.com>
2025-07-17 23:23:03 +0200

    Initial code

---
+++ GuidMCPServer/Tools/GuidCreationTools.cs
@@ -0,0 +1,28 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+namespace GuidMCPServer.Tools;
+
+internal class GuidCreationTools
+{
+    [McpServerTool]
+    [Description("Generates a new GUID in the format of a version 4 UUID.")]
+    public string GetGuidv4()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+    [McpServerTool]
+    [Description("Generates a new GUID in the format of a version 7 UUID.")]
+    public string GetGuidv7()
+    {
+        return Guid.CreateVersion7().ToString();
+    }
+
+    [McpServerTool]
+    [Description("Generates a new GUID in the format of a version 7 UUID for the given Datetime offset.")]
+    public string GetGuidv7ByDateTimeOffset([Description("DateTime Offset of the UUID v7")]DateTimeOffset datetimeOffset)
+    {
+        return Guid.CreateVersion7(datetimeOffset).ToString();
+    }
+}
